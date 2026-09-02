155aea566f483ce99f7c6087ec9f87f7e562b140
박소윤 <ysop@kbiohc.io>
2026-09-02 16:22:52 +0900

    chore: 실행 테스트용 기본 데이터 추가

--- src/main/java/org/fastcampus/student_management/Main.java
+++ src/main/java/org/fastcampus/student_management/Main.java
@@ -1,7 +1,9 @@
 package org.fastcampus.student_management;
 
 import org.fastcampus.student_management.application.course.CourseService;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
 import org.fastcampus.student_management.repo.CourseRepository;
 import org.fastcampus.student_management.repo.StudentRepository;
 import org.fastcampus.student_management.ui.course.CourseController;
@@ -24,6 +26,18 @@ public class Main {
 
     CourseController courseController = new CourseController(coursePresenter, courseService, studentPresenter);
     StudentController studentController = new StudentController(studentPresenter, studentService);
+
+    // 기본 default 세팅 추가
+    StudentInfoDto studentInfoDto = new StudentInfoDto("홍길동", 20, "서울시 강남구");
+    StudentInfoDto studentInfoDto1 = new StudentInfoDto("김연아", 24, "서울시 노원구");
+    studentService.saveStudent(studentInfoDto);
+    studentService.saveStudent(studentInfoDto1);
+
+    CourseInfoDto courseInfoDto = new CourseInfoDto("바이올린", 1000, "MONDAY", "홍길동", 1717299888L);
+    CourseInfoDto courseInfoDto1 = new CourseInfoDto("첼로", 1200, "MONDAY", "김연아", 1717299888L);
+    courseService.registerCourse(courseInfoDto);
+    courseService.registerCourse(courseInfoDto1);
+
 
     studentPresenter.showMenu();
     UserInputType userInputType = studentController.getUserInput();
