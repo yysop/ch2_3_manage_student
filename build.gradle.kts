4e362972f4be6c4b1ff68ac229956bbcc75218f8
박소윤 <ysop@kbiohc.io>
2026-09-02 15:02:52 +0900

    chore: Java UTF-8 설정 및 빌드 파일 제외

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,10 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+// Java 소스의 한글 출력 -> Gradle 실행 콘솔 과정에서 문자 인코딩이 맞지 않아 설정 추가
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+
